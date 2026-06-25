6c1b194fc83eac5d3245f5b26812ffd42afc8b3d
idelri <idelri91@gmail.com>
2026-06-25 14:38:08 +0200

    Update Planificacion.jsx

--- src/pages/Planificacion.jsx
+++ src/pages/Planificacion.jsx
@@ -1311,7 +1311,7 @@ const pctKm = kmObjetivoMedio && kmRealMedio > 0 ? Math.round((kmRealMedio / kmO
                                           </div>
                                         )}
                                       </div>
-                                  )}
+                                    )}
                                   </div>
                                 </div>
                               )}
