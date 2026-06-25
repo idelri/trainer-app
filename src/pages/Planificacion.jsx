48f6d00c1ed9ae80c78fb613d4426a337697b033
idelri <idelri91@gmail.com>
2026-06-25 14:49:06 +0200

    Update Planificacion.jsx

--- src/pages/Planificacion.jsx
+++ src/pages/Planificacion.jsx
@@ -1235,9 +1235,9 @@ const pctKm = kmObjetivoMedio && kmRealMedio > 0 ? Math.round((kmRealMedio / kmO
                                             )}
                                           </div>
                                         </div>
-                                      )
-                                   })()}
-                                    {clienteData?.perfil_planificacion === 'fuerza_salud' && (sub.sesiones_min || sub.sesiones_max || sub.duracion_media_min || sub.exigencia || sub.enfoque?.length > 0) && (
+                                    )
+                                    })()}
+                                    {clienteData?.perfil_planificacion === 'fuerza_salud' &&
                                       <div style={{ display: 'flex', flexDirection: 'column', gap: 10 }}>
                                         {(sub.sesiones_min || sub.sesiones_max) && (
                                           <div>
